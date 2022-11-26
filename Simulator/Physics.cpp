f0da59369ef49f400d7c977649fd9c5a1ec07e2d
Atharva-Gondhali <atharvagondhali24@gmail.com>
2022-11-26 23:50:54 +0530

    Line physics done (rendering)

--- Simulator/Physics.cpp
+++ Simulator/Physics.cpp
@@ -4,85 +4,24 @@ void Physics::initWindow()
 {
 	this->videoMode.height = 720;
 	this->videoMode.width = 1280;
-	this->window = new sf::RenderWindow(this->videoMode, "Simulator", sf::Style::Titlebar | sf::Style::Close);
+	this->settings.antialiasingLevel = 8;
+	this->window = new sf::RenderWindow(this->videoMode, "Simulator", sf::Style::Titlebar | sf::Style::Close, settings);
 	this->window->setFramerateLimit(60);
 }
 
 void Physics::initVariable()
 {
 	this->ball.setRadius(10.f);
+
 	vel = 27.7f;
 	angle = 45.f;
 	velX = vel * cos(angle * (3.14f / 180.f));
 	velY = -(vel * sin(angle * (3.14f / 180.f)));
-	gravity = 9.8f;
-	mass = 10.f;
-	diameter = 0.08f;
-	densityAir = 1.2f;
-}
-
-void Physics::initAcceln()
-{
-	float reynolds, forceGravity, forceDrag;
-
-	forceGravity = mass * gravity;
-
-	reynolds  = (densityAir * diameter * velX) / viscosity;
-	if (reynolds > 1)
-	{
-		forceDrag = 0.5f * densityAir * dragCoeff * (pi * powf(diameter / 2.f, 2.f)) * (powf(velX, 2.f));
-	}
-	else
-	{
-		forceDrag = 6.f * pi * viscosity * (diameter / 2.f) * velX;
-	}
-	verticalAcceln = -((forceGravity / mass) / 10);
-	horizontalAcceln = -((forceDrag / mass) / 10);
-}
-
-void Physics::projectCalc()
-{
-	// v sqrt(2a + u2)
-	float t_velX, t_velY;
-	if (velX < 0)
-	{
-		t_velX = -powf(velX, 2.f) + (2.f * horizontalAcceln);
-	}
-	else
-	{
-		t_velX = powf(velX, 2.f) + (2.f * horizontalAcceln);
-	}
-	if (velY < 0)
-	{
-		t_velY = -powf(velY, 2.f) - (2.f * verticalAcceln) ;
-	}
-	else
-	{
-		t_velY = powf(velY, 2.f) - (2.f * verticalAcceln) ;
-	}
-	
-	if (t_velX > 0) 
-	{
-		velX = sqrt(t_velX);
-	}
-	else
-	{
-		velX = -sqrt(-t_velX);
-	}
-	if (t_velY > 0)
-	{
-		velY = sqrt(t_velY);
-	}
-	else
-	{
-		velY = -sqrt(-t_velY);
-	}
 }
 
 Physics::Physics()
 {
 	this->initVariable();
-	this->initAcceln();
 	this->initWindow();
 
 	this->spawnBall();
@@ -102,26 +41,43 @@ void Physics::pollEvents()
 		case sf::Event::Closed:
 			this->window->close();
 			break;
-		}
+ 		}
+	}
+}
+
+void Physics::linePhy()
+{
+	this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+	this->mousePosView = this->window->mapPixelToCoords(this->mousePosWindow);
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		this->line[0].position = this->mousePosView;
+		this->line[1].position = sf::Vector2f(this->ball.getPosition().x + 10.f, this->ball.getPosition().y + 10.f);
+		this->line[0].color = ball.getFillColor();
+		this->line[1].color = sf::Color::White;
+	}
+	else
+	{
+		this->line[0].position = sf::Vector2f(0.f, 0.f);
+		this->line[1].position = sf::Vector2f(0.f, 0.f);
+		this->line->color = sf::Color::Black;
 	}
 }
 
 void Physics::spawnBall()
 {
-	this->ball.setPosition(0.f, 720.f);
-	this->ball.setFillColor(sf::Color::Blue);
+	this->ball.setPosition(100.f, 100.f);
+	this->ball.setFillColor(sf::Color::Red);
 }
 
 void Physics::updateBall()
 {
-	std::cout << velX << " " << velY << "\n";
-	projectCalc();
-	this->ball.move(0.167f * velX, 0.167f * velY);
 }
 
 void Physics::update()
 {
 	this->pollEvents();
+	this->linePhy();
 	this->updateBall();
 }
 
@@ -133,6 +89,7 @@ void Physics::renderBall()
 void Physics::render()
 {
 	this->window->clear();
+	this->window->draw(line, 2, sf::Lines);
 	this->renderBall();
 	this->window->display();
 }
--- Simulator/Physics.h
+++ Simulator/Physics.h
@@ -13,7 +13,12 @@ private:
 	sf::RenderWindow* window;
 	sf::VideoMode videoMode;
 	sf::Event ev;
+	sf::ContextSettings settings;
 
+	sf::Vector2i mousePosWindow;
+	sf::Vector2f mousePosView;
+
+	sf::Vertex line[2];
 	sf::CircleShape ball;
 
 	float vel;
@@ -21,41 +26,27 @@ private:
 	float velY;
 	float angle;
 
-
-	//Gravity
-	float gravity;
-	float mass;
-
-	//Drag
-	float diameter;
-	float densityAir;
-
-	//Acceln
-	float verticalAcceln;
-	float horizontalAcceln;
-
 	const float pi = 3.14f;
 	const float viscosity = 0.00001f;
 	const float dragCoeff = 0.47f;
 
 	void initWindow();
 	void initVariable();
-	void initAcceln();
-	
-	void projectCalc();
+
+
+	void pollEvents();
+	void linePhy();
+	void spawnBall();
+	void updateBall();
+	void renderBall();
 
 public:
 	Physics();
 
 	const bool running() const;
 
-	void pollEvents();
-
-	void spawnBall();
-	void updateBall();
 	void update();
 
-	void renderBall();
 	void render();
 
 };
